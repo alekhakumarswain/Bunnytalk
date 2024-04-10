53743f02a7ae40432703daae0157ce9eeecc74bd
Alekha Kumar Swain <51305914+alekhakumarswain@users.noreply.github.com>
2024-04-11 01:47:03 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyB4PXatCRA7I0Uvc8O2IXs13mbfY8NK-gY",
+    authDomain: "bunnytalk-db.firebaseapp.com",
+    projectId: "bunnytalk-db",
+    storageBucket: "bunnytalk-db.appspot.com",
+    messagingSenderId: "79115378372",
+    appId: "1:79115378372:web:e10a65a0f40b4a9448b391",
+    measurementId: "G-G4MWQCPKER"
+};
+firebase.initializeApp(config);
