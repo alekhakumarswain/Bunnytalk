a5b47f3555b145c68339d9aa413d2774db2593b4
Alekha Kumar Swain <51305914+alekhakumarswain@users.noreply.github.com>
2024-06-05 02:03:14 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,43 @@
 var cacheName = 'bunnytalk-rabbit';
 var filesToCache = [
     '/',
-    '/index.html',  
-    '/manifest.json', 
-    '/background.js',
-    '/chat.html',
-    '/config.js',
+    '/index.html', 
+    '/chat.html', 
     '/gallery.html',
-    '/images.json',
-    '/login.html',
     '/profile.html',
-    '/skeleton.css',
-    '/skeleton.js',
-    '/users.json'
+    '/login.html',
+    '/css/skeleton.css',
+    '/js/background.js',
+    '/js/config.js',
+    '/js/dateformat.js',
+    '/js/skeleton.js',
+    '/404.html',
+    '/js/images.json',
+    '/js/users.json',
+    '/manifest.json', 
 ];
 
 self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open(cacheName).then(function(cache) {
-            return cache.addAll(filesToCache.map(file => new Request(file, { cache: 'reload' })))
-                .catch(function(error) {
-                    console.error('Failed to cache files:', error);
-                    throw error;
-                });
+            return Promise.all(
+                filesToCache.map(function(file) {
+                    return fetch(new Request(file, { cache: 'reload' })).then(function(response) {
+                        if (response.ok) {
+                            return cache.put(file, response);
+                        } else {
+                            console.error('Failed to fetch ' + file + ': ' + response.statusText);
+                            throw new TypeError('Bad response status');
+                        }
+                    }).catch(function(error) {
+                        console.error('Request for ' + file + ' failed:', error);
+                        throw error;
+                    });
+                })
+            );
+        }).catch(function(error) {
+            console.error('Failed to open cache:', error);
+            throw error;
         })
     );
 });
